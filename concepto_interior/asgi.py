5e14659be49428fed07dc1803638bde8e11a3899
belli29 <belli29@hotmail.com>
2021-01-05 18:50:06 +0000

    my initial commit

---
+++ concepto_interior/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for concepto_interior project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'concepto_interior.settings')
+
+application = get_asgi_application()
