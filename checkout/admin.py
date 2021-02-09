039abab65190a0e9edd055ec1b5302797d1e0fc3
belli29 <belli29@hotmail.com>
2021-02-09 22:37:16 +0000

    amended OxxoOrderAdmin model

--- checkout/admin.py
+++ checkout/admin.py
@@ -76,14 +76,14 @@ class OxxoOrderAdmin(admin.ModelAdmin):
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
                        'grand_total', 'upgraded_order',
-                       'stripe_pid')
+                       'stripe_pid', 'original_bag')
 
     fields = ('order_number', 'upgraded_order', 'status',
               'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', "stripe_pid", 'original_bag')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
