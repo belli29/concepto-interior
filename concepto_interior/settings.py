152e7cc391c2e6ee5ba005c6dd6667b4cb1230ae
ste <belli29@hotmail.com>
2021-09-06 22:45:33 +0200

    allowing new host in settings

--- concepto_interior/settings.py
+++ concepto_interior/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['concepto-interior.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['concepto-interior.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
